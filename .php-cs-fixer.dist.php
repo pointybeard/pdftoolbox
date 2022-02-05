6822796db3d0a60240fb2d993ed692c047fd10c0
Alannah Kearney <alannah@clueydigital.com.au>
2022-02-05 12:30:09 +0800

    chore: Updated copyright header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $header = <<<EOF
 This file is part of the "PHP Wrapper for callas pdfToolbox" repository.
 
-Copyright 2021 Alannah Kearney <hi@alannahkearney.com>
+Copyright 2021-2022 Alannah Kearney <hi@alannahkearney.com>
 
 For the full copyright and license information, please view the LICENCE
 file that was distributed with this source code.
